ef62deed7a8d79f629b0b5362f464de2c9be82a4
Farhan Syed <farhansyed11.3@gmail.com>
2017-04-15 13:30:08 -0700

    Updated to Swift 3

--- Enumerations.playground/Contents.swift
+++ Enumerations.playground/Contents.swift
@@ -14,14 +14,10 @@ enum name {
 // Our Basic Enumeration
 
 enum Levels {
- case One
- case Two
- case Three
- case Four
-    
-    init() {
-        self = .One
-    }
+ case one
+ case two
+ case three
+ case four
 }
 
 
@@ -29,7 +25,7 @@ enum Levels {
 // Like so:
 
 enum anotherWayToWrite {
-    case One, Two, Three, Four
+    case one, two, three, four
 }
 
 // Set variable to a enumeration case
@@ -38,15 +34,15 @@ enum anotherWayToWrite {
 
 // or even simpler
 
-var playerLevel = Levels.Two
+var playerLevel = Levels.two
 
 // You can do a Switch statement for each case. If you don't understand Switch, take a look at my article here: https://medium.com/@farhansyed/switch-cases-in-swift-29366716242d
 
 switch playerLevel {
-case Levels.One: print("You're on level 1, keep it up.")
-case Levels.Two: print("You're on level 2, keep going!")
-case Levels.Three: print("You're on level 3, almost there!")
-case Levels.Four: print("Finally, you made it to level 4. You won!")
+case Levels.one: print("You're on level 1, keep it up.")
+case Levels.two: print("You're on level 2, keep going!")
+case Levels.three: print("You're on level 3, almost there!")
+case Levels.four: print("Finally, you made it to level 4. You won!")
 }
 
 // Quick Tip: 
@@ -54,10 +50,10 @@ case Levels.Four: print("Finally, you made it to level 4. You won!")
 // Typing less is always nice 😊
 
 switch playerLevel {
-case .One: print("You're on level 1, keep it up.")
-case .Two: print("You're on level 2, keep going!")
-case .Three: print("You're on level 3, almost there!")
-case .Four: print("Finally, you made it to level 4. You won!")
+case .one: print("You're on level 1, keep it up.")
+case .two: print("You're on level 2, keep going!")
+case .three: print("You're on level 3, almost there!")
+case .four: print("Finally, you made it to level 4. You won!")
 }
 
 // Instead of having the values of each level inside the switch statement, we can actually store them in the enum with something call "raw values"
@@ -65,13 +61,13 @@ case .Four: print("Finally, you made it to level 4. You won!")
 // Let's create a new enum:
 
 enum Companies: String {
-case Apple = "Apple was founded on April 1, 1976 in Cupertino, CA"
-case Amazon = "Amazon was founded on July 5, 1994 in Seattle, WA"
-case Google = "Google was founded on September 4, 1998 in Menlo Park, CA"
-case Microsoft = "Microsoft was founded on April 4, 1975 in Albuquerque, NM"
+case apple = "Apple was founded on April 1, 1976 in Cupertino, CA"
+case amazon = "Amazon was founded on July 5, 1994 in Seattle, WA"
+case google = "Google was founded on September 4, 1998 in Menlo Park, CA"
+case microsoft = "Microsoft was founded on April 4, 1975 in Albuquerque, NM"
 }
 
-var company = Companies.Apple
+var company = Companies.apple
 // now we can grab the raw value
 
 print(company.rawValue)
